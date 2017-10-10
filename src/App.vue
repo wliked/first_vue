44b8b4ddeda5567af0164eb5df3a277a3eaf9942
liang <liang.wang@dianrong.com>
2017-10-10 19:05:38 +0800

    add base vue App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div id="wrapper">
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <i class="glyphicon glyphicon-time"></i>
+            计划板
+          </a>
+          <ul class="nav navbar-nav">
+            <li><router-link to="/home">首页</router-link></li>
+            <li><router-link to="/time-entries">计划列表</router-link></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="col-sm-3">
+           <sidebar></sidebar>
+        </div>
+        <div class="col-sm-9">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    import Sidebar from './components/Sidebar.vue'
+
+    export default {
+      name: 'app',
+      components: {'sidebar': Sidebar}
+    }
 </script>
 
 <style>
